7f581aefd21b61a81a79f015ec14da01f4b2ea54
SriD647 <sridatt.rao@hotmail.com>
2023-09-18 20:20:41 -0400

    updated stream links

--- src/components/MovieDescription.js
+++ src/components/MovieDescription.js
@@ -48,10 +48,14 @@ export default function MovieDescription({ movie }) {
           <strong>IMDb Rating:</strong> <i>{item.imdbRating}</i>
         </p>
         <p>
-          <strong>Stream link:</strong>{" "}
-          <a  href={`https://www2.solarmovie.cr/search/${movie.Title.toLowerCase().split(" ").join("%20")}/`}target="_blank"> Link
+          <strong>Stream:</strong>{" "}
+          <a  href={`https://movie4kto.net/search/${movie.Title.toLowerCase().split(" ").join("-")}`}target="_blank"> Link 1
           </a>
-
+        </p>
+        <p>
+          <strong>Stream:</strong>{" "}          
+          <a  href={`https://www2.solarmovie.cr/search/${movie.Title.toLowerCase().split(" ").join("%20")}/`}target="_blank"> Link 2
+          </a>
         </p>
       </nav>
     </div>
