afefba2f005e5b717d5e3e4289e2ba44e61b736e
SriD647 <sridatt.rao@hotmail.com>
2023-09-25 18:26:13 -0400

    npm run build

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 8913;
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
